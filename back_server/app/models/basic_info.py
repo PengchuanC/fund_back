0b0b01ea8762853632398bda2d351964c8e54050
chuanchao.peng <pengchuan@outlook.om>
2019-08-22 15:18:59 +0800

    Initial commit

--- back_server/app/models/basic_info.py
+++ back_server/app/models/basic_info.py
@@ -19,5 +19,20 @@ class BasicInfo(db.Model):
     `KIND_II` char(20)
     );
     """
+    __tablename__ = "basic_info"
     id = db.Column(db.Integer, primary_key=True)
     windcode = db.Column(db.String(10))
+    sec_name = db.Column(db.String(50))
+    fund_fullname = db.Column(db.String(100))
+    fund_benchmark = db.Column(db.TEXT)
+    fund_investscope = db.Column(db.TEXT)
+    fund_structuredfundornot = db.Column(db.String(2))
+    fund_setupdate = db.Column(db.DATETIME)
+    fund_firstinvesttype = db.Column(db.String(25))
+    fund_investtype = db.Column(db.String(25))
+    type = db.Column(db.String(5))
+    kind_i = db.Column(db.String(20))
+    kind_ii = db.Column(db.String(20))
+
+    def __repr__(self):
+        return f"<BasicInfo {self.sec_name}>"
